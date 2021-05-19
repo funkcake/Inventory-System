f19c492fd1fd894c24f8b688d81d4f3e98803f38
Micheal Ellis <76069321+MichealJWEllis@users.noreply.github.com>
2021-05-19 14:05:36 -0400

    changed schema file

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,6 @@
 -- DROP DATABASE
-DROP DATABASE IF EXISTS ecommerce_db;
+DROP DATABASE IF EXISTS itinventory_db;
 
 -- CREATE DATABASE
-CREATE DATABASE ecommerce_db;
+CREATE DATABASE itinventory_db;
 
